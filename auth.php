b033b61e5fb40f5826b9ba2398d41a709a421702
Gianluca Mazzini <gianluca@mazzini.org>
2026-02-11 10:41:26 +0100

    Update auth.php

--- auth.php
+++ auth.php
@@ -2,7 +2,8 @@
 
 function myauth($sez,$cell){
   $otp=sprintf("%05d",rand(0,99999));
-  echo "<span id='yyy'>Sezione $sez, Utente $cell<br>Invia via whatapp il codice <b>$otp</b> al numero di autenticazione 3770867586 entro 90 secondi<br><span style='color:red;'>In alternativa e in via fortemente sperimentale puoi inviare via SMS il codice <b>$otp</b> al numero di autenticazione 3793416173 entro 90 secondi</span>\n</span>";
+  echo "<span id='yyy'>Sezione $sez, Utente $cell<br>Invia via whatapp il codice <b>$otp</b> al numero di autenticazione 3770867586 entro 90 secondi<br></span>";
+  // echo "<span style='color:red;'>Altrimenti e in via sperimentale invia via SMS il codice <b>$otp</b> al numero di autenticazione 3793416173 entro 90 secondi</span>";
   ob_flush(); flush();
   $epoch=time();
   $look=0;
